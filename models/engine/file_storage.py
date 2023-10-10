1207e9e67328a135b40368c12cd8901218acbdd5
affr117 <alfredo.figueroa07@yahoo.com>
2023-10-10 16:31:11 -0700

    correct out different code json correct out as well all other files not adding

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,19 +19,30 @@ class FileStorage:
         FileStorage.__objects[key] = obj
 
     def save(self):
+        #print("hello in save")
         serializable_objects = {}
-
+    
         for key, value in self.__objects.items():
             # Convert the object's dictionary to a serializable format
-            obj_dict = value.to_dict()
+            obj_dict = value.copy()  # Assuming the value is a dictionary
+            if 'created_at' in obj_dict and isinstance(obj_dict['created_at'], datetime.datetime):
+                obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+            if 'updated_at' in obj_dict and isinstance(obj_dict['updated_at'], datetime.datetime):
+                obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+
             serializable_objects[key] = obj_dict
 
         with open(self.__file_path, 'w') as file:
-            json.dump(serializable_objects, file)
+            json.dump(serializable_objects, file) 
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
-        if os.path.isfile(FileStorage.__file_path):
+        try:
+            with open(self.__file_path, 'r') as file:
+                self.__objects = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError): 
+            pass
+        """if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 for key, value in data.items():
@@ -42,4 +53,4 @@ class FileStorage:
                         value['updated_at'] = datetime.datetime.strptime(value['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
                     class_name, obj_id = key.split('.')
                     obj = BaseModel(**value)
-
+"""
