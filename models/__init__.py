78d1ce68830c771f487448176aae7a0996b6b2ad
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-25 11:30:46 -0700

    update task 0 correct out

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from .engine.file_storage import FileStorage
-
-print("Running as root:", os.geteuid() == 0)
-
-# Create a unique FileStorage instance for your application
-storage = FileStorage()
-storage.reload()
-
--- models/base_model.py
+++ models/base_model.py
@@ -1,85 +1,24 @@
 #!/usr/bin/python3
-"""
-Module Doc: Base Class for all other classes
-"""
 
-import json
-import datetime
 import uuid
-import os
+from datetime import datetime
 
 class BaseModel:
-    """
-    class Doc: Base Class
-    """
-    __nb_objects = 0
-
-    def __init__(self, *args, **kwargs):
-        """
-        function Desc:  init
-        """
-        from models import storage  # Import here to avoid circular import
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ("created_at", "updated_at") and isinstance(value, str):
-                        setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-        else:
-            BaseModel.__nb_objects += 1
-            self.my_number = BaseModel.__nb_objects
-            self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at
-            self.id = str(uuid.uuid4())
-            storage.new(self)
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        """Override string to provide a better description."""
-        return f"[{self.__class__.__name__}]({self.id}) {self.to_dict()}"
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """update public instance attribute updated_at by the current datetime"""
-        from models import storage  # Import here to avoid circular import
-        self.updated_at = datetime.datetime.now()
-        storage.save()  # Use the storage module here
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary containing keys/values of the instance attributes."""
-        dict_rep = self.__dict__.copy()
-        dict_rep['__class__'] = self.__class__.__name__
-        dict_rep['created_at'] = self.created_at.isoformat()
-        dict_rep['updated_at'] = self.updated_at.isoformat()
-        dict_rep['id'] = self.id
-        dict_rep['my_number'] = self.my_number
-        dict_rep['name'] = self.name
-        return dict_rep
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My_First_Model"
-    my_model.my_number = 89
-    my_model.save()
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-
-    print(my_model is my_new_model)
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
