77bc68f7758f2dfd63cc10c953c6ca4efbdfbdb6
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-25 12:42:31 -0700

    updated correct order and format task 0 classes corrected

--- models/base_model.py
+++ models/base_model.py
@@ -30,20 +30,34 @@ class BaseModel:
 
     def to_dict(self):
         """
-        Returns a dictionary representation of the BaseModel instance with the desired key order.
+        Returns a dictionary representation of the BaseModel instance with the desired key order and ISO format for timestamps.
         """
         class_name = self.__class__.__name__
         key_order = ['my_number', 'name', '__class__', 'updated_at', 'id', 'created_at']
-        
+
+    # Format timestamps to ISO format strings
+        updated_at_iso = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+        created_at_iso = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+
+    # Convert class types to strings
+        class_type_str = str(self.__class__)
+        updated_at_type_str = str(type(self.updated_at))
+        created_at_type_str = str(type(self.created_at))
+
+    # Construct ordered dictionary
         ordered_dict = {
-            '__class__': class_name,
+            '__class__': class_type_str,
             'id': self.id,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat(),
+            'created_at': created_at_iso,
+            'updated_at': updated_at_iso,
+            '__class__': class_type_str,
+            '__updated_at__': updated_at_type_str,
+            '__created_at__': created_at_type_str,
             **self.__dict__
-        }
-        
-        # Create a dictionary with the desired key order
+    }
+
+    # Create a dictionary with the desired key order
         sorted_dict = {k: ordered_dict[k] for k in key_order}
         return sorted_dict
+
 
