d777e9e13e89aeb85b1efa37f6c8d913c4b24832
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-25 11:51:34 -0700

    correct order out task 0

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,49 @@
 #!/usr/bin/python3
-
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """
+    This is the BaseModel class that defines common attributes/methods for other classes.
+    """
     def __init__(self):
+        """
+        Initializes a new BaseModel instance with a unique ID and timestamps.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        Returns a string representation of the BaseModel instance.
+        Format: "[<class name>] (<self.id>) <self.__dict__>"
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates the 'updated_at' attribute with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """
+        Returns a dictionary representation of the BaseModel instance with the desired key order.
+        """
+        class_name = self.__class__.__name__
+        key_order = ['my_number', 'name', '__class__', 'updated_at', 'id', 'created_at']
+        
+        ordered_dict = {
+            '__class__': class_name,
+            'id': self.id,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+            **self.__dict__
+        }
+        
+        # Create a dictionary with the desired key order
+        sorted_dict = {k: ordered_dict[k] for k in key_order}
+        return sorted_dict
 
